f3fa930b517452858e7578a5d75e743c7a0f0ca8
andrescallejasg <andres.callejasg@gmail.com>
2023-06-18 17:15:00 -0500

    builtins

--- main.go
+++ main.go
@@ -14,7 +14,11 @@ func main() {
 
 	for {
 
-		fmt.Print("$ ")
+		// print current dir and '$'
+		path, _ := os.Getwd()
+		fmt.Printf("%v$ ", path)
+
+		// read from standard input
 		buf := bufio.NewReader(os.Stdin)
 		cmdInput, err := buf.ReadString('\n')
 		if err != nil {
@@ -23,29 +27,42 @@ func main() {
 
 		cmdInput = strings.TrimSuffix(cmdInput, "\n")
 
+		//Execute command
 		exitFlag := execCommands(cmdInput)
 		if exitFlag {
 			break
 		}
-
 	}
 
 }
 
 func execCommands(cmdInput string) (ExitFlag bool) {
 
-	if cmdInput == "exit" {
-		return true
-	}
-
+	// if cmdInput == "" || cmdInput == " " {
+	// 	return false
+	// }
 	arrayCmd := strings.Fields(cmdInput)
-	commandExec := exec.Command(arrayCmd[0], arrayCmd[1:]...)
-	commandExec.Stderr = os.Stderr
-	commandExec.Stdout = os.Stdout
-	err := commandExec.Run()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	if len(arrayCmd) < 1 {
+		return false
 	}
 
+	switch arrayCmd[0] {
+	case "exit":
+		return true
+	case "help":
+		help()
+	case "cd":
+		cd(arrayCmd)
+	case "echo":
+		echo(arrayCmd)
+	default:
+		commandExec := exec.Command(arrayCmd[0], arrayCmd[1:]...)
+		commandExec.Stderr = os.Stderr
+		commandExec.Stdout = os.Stdout
+		err := commandExec.Run()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
 	return false
 }
